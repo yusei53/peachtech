6e6840e61bdfdaf8e09affb9df742ad008735672
sora-00 <sora.umiushi@gmail.com>
2024-05-17 00:37:44 +0900

    cardContentDataの作成

---
+++ app/const/card-content-data.ts
@@ -0,0 +1,38 @@
+type cardContentDataProps = {
+  src: string;
+  title: string;
+  content: string;
+};
+
+export const cardContentData: cardContentDataProps[] = [
+  {
+    src: "./pinatsu.png",
+    title: "あいうえお",
+    content: "あいうえお",
+  },
+  {
+    src: "./pinatsu.png",
+    title: "あいうえ",
+    content: "あいうえ",
+  },
+  {
+    src: "./pinatsu.png",
+    title: "あいうえお",
+    content: "あいうえお",
+  },
+  {
+    src: "./pinatsu.png",
+    title: "あいうえ",
+    content: "あいうえ",
+  },
+  {
+    src: "./pinatsu.png",
+    title: "あいうえお",
+    content: "あいうえお",
+  },
+  {
+    src: "./pinatsu.png",
+    title: "あいうえ",
+    content: "あいうえ",
+  },
+];
