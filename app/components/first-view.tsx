4fc8005b1bb2ea520920945f896f69d934aa97d6
yusei53 <yusei530922@gmail.com>
2024-05-16 14:10:21 +0900

    FirstViewコンポーネントをリアクタリング

--- app/components/first-view.tsx
+++ app/components/first-view.tsx
@@ -1,8 +1,7 @@
 "use client";
-
 import { Box, Typography } from "@mui/material";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Autoplay, Navigation, Pagination } from "swiper/modules";
+import { Autoplay, Pagination } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
@@ -13,60 +12,49 @@ const FirstView = () => {
 
   return (
     <Box
-      height={{ xs: "50vh", md: "70vh", lg: "88vh" }}
-      display="flex"
-      justifyContent="center"
-      alignItems={{ xs: "start", lg: "center" }}
+      display={"flex"}
+      alignItems={"center"}
+      flexDirection={{ xs: "column-reverse", md: "row" }}
+      my={{ md: 5 }}
     >
-      <Box
-        display="flex"
-        flexDirection={{ xs: "column-reverse", lg: "row" }}
-        width="100%"
+      <Typography
+        component="h2"
+        px={{ md: 5, lg: 10 }}
+        pt={15}
+        fontSize={{ xs: 20, md: 28 }}
       >
-        <Typography
-          component="h4"
-          whiteSpace="pre-wrap"
-          px={{ md: 5, lg: 10 }}
-          py={12}
-          display="flex"
-          alignItems="end"
-          fontSize={{ md: 40, lg: 20 }}
-          textAlign="center"
-          justifyContent={{ xs: "center", lg: "normal" }}
+        明日を創造する
+        <br />
+        第一歩をともに
+      </Typography>
+      <Box
+        width={{ xs: "100%", md: "65%" }}
+        height={{ xs: "40vh", md: "70vh" }}
+        ml={{ md: 5 }}
+      >
+        <Swiper
+          modules={[Pagination, Autoplay]}
+          loop={true}
+          autoplay={{
+            delay: 3000,
+            disableOnInteraction: false,
+          }}
+          pagination={{
+            clickable: true,
+          }}
+          style={{ width: "100%", height: "100%", borderRadius: "10px" }}
         >
-          {"明日を創造する\n第一歩をともに"}
-        </Typography>
-        <Box
-          width={{ xs: "100%", md: "75%" }}
-          height={{ xs: "40vh", sm: "50vh", lg: "80vh" }}
-        >
-          <Swiper
-            modules={[Pagination, Navigation, Autoplay]}
-            spaceBetween={30}
-            centeredSlides={true}
-            loop={true}
-            autoplay={{
-              delay: 3000,
-              disableOnInteraction: false,
-            }}
-            pagination={{
-              clickable: true,
-            }}
-            navigation={true}
-            style={{ width: "100%", height: "100%", borderRadius: "10px" }}
-          >
-            {images.map((src, index) => (
-              <SwiperSlide key={index}>
-                <Image
-                  fill
-                  src={src}
-                  alt={`Slide ${index}`}
-                  style={{ objectFit: "cover" }}
-                />
-              </SwiperSlide>
-            ))}
-          </Swiper>
-        </Box>
+          {images.map((src, index) => (
+            <SwiperSlide key={index}>
+              <Image
+                fill
+                src={src}
+                alt={`Slide ${index}`}
+                style={{ objectFit: "cover" }}
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </Box>
     </Box>
   );
