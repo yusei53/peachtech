5343043c52c3e107f708e617bc62bb41bdc91b68
sora-00 <sora.umiushi@gmail.com>
2024-05-24 16:53:58 +0900

    activity-achievementを作成

---
+++ app/components/achievement/activity-achievement.tsx
@@ -0,0 +1,34 @@
+import { Box, Divider, Grid } from "@mui/material";
+import Heading from "../common/heading";
+import { achievementContentData } from "@/app/const/achievement-data";
+import AchievementCard from "./achievement-card";
+import React from "react";
+
+const ActivityAchievement = () => {
+  return (
+    <Box py={13} px={{ xs: 5, md: 5 }}>
+      <Heading title="活動実績" />
+
+      <Grid container spacing={{ xs: 2, md: 10 }} pt={15}>
+        {achievementContentData.map((data, index) => (
+          <React.Fragment key={index}>
+            <Grid item md={12} display={"flex"} justifyContent={"center"}>
+              <AchievementCard
+                src={data.src}
+                date={data.date}
+                title={data.title}
+                content={data.content}
+              />
+            </Grid>
+
+            <Grid md={12} mt={8} display={"flex"} justifyContent={"center"}>
+              <Divider sx={{ width: "80%" }} color={"black"} />
+            </Grid>
+          </React.Fragment>
+        ))}
+      </Grid>
+    </Box>
+  );
+};
+
+export default ActivityAchievement;
