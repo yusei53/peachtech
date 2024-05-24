df0450bcc3a1663030ffc2a96d16a785b9408f40
sora-00 <sora.umiushi@gmail.com>
2024-05-24 16:55:05 +0900

    achievement-dataを作成

---
+++ app/const/achievement-data.ts
@@ -0,0 +1,47 @@
+export type achievementDataProps = {
+  src: string;
+  date: string;
+  title: string;
+  content: string;
+};
+
+export const achievementContentData: achievementDataProps[] = [
+  {
+    src: "/gray.png",
+    date: "2024.5.22",
+    title: "あいうえお",
+    content:
+      "活動実績活動実績活動実績活動実績活動実績活動実績活動実績活動実績活動実績活動実績活動実績活動実績活動実績活動実績活動実績活動実績活動実績活動実績活動実績活動実績活動実績活動実績活動実績活動実績活動実績活動実績活動実績活動実績",
+  },
+
+  {
+    src: "/gray.png",
+    date: "2024.5.22",
+    title: "あいうえ",
+    content: "あいうえ",
+  },
+  {
+    src: "/gray.png",
+    date: "2024.5.22",
+    title: "あいうえお",
+    content: "あいうえお",
+  },
+  {
+    src: "/gray.png",
+    date: "2024.5.22",
+    title: "あいうえ",
+    content: "あいうえ",
+  },
+  {
+    src: "/gray.png",
+    date: "2024.5.22",
+    title: "あいうえお",
+    content: "あいうえお",
+  },
+  {
+    src: "/gray.png",
+    date: "2024.5.22",
+    title: "あいうえ",
+    content: "あいうえ",
+  },
+];
