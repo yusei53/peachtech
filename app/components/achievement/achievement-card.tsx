59a799f5400edffa18a2f9408529270b481975ee
sora-00 <sora.umiushi@gmail.com>
2024-05-25 00:06:40 +0900

    活動実績レスポンシブ対応

--- app/components/achievement/achievement-card.tsx
+++ app/components/achievement/achievement-card.tsx
@@ -11,10 +11,32 @@ const AchievementCard: React.FC<achievementDataProps> = ({
 }) => {
   return (
     <Box display={"flex"} width={"80%"}>
-      <Box>
-        <Image width={450} height={300} src={src} alt="" />
+      <Box
+        width={{ xs: 360, md: 450 }}
+        height={{ xs: 240, md: 300 }}
+        position={"relative"}
+        display={"flex"}
+        alignItems={"center"}
+      >
+        <Image
+          width={450}
+          height={300}
+          sizes="100vw"
+          style={{
+            width: "100%",
+            height: "auto",
+          }}
+          src={src}
+          alt=""
+        />
       </Box>
-      <Box px={7} display={"flex"} flexDirection={"column"} gap={2}>
+      <Box
+        width={{ xs: 450, md: 600 }}
+        px={7}
+        display={"flex"}
+        flexDirection={"column"}
+        gap={2}
+      >
         <Typography fontSize={{ xs: 10, md: 20 }}>{date}</Typography>
         <Typography
           gutterBottom
@@ -23,7 +45,7 @@ const AchievementCard: React.FC<achievementDataProps> = ({
         >
           {title}
         </Typography>
-        <Typography fontSize={{ xs: 10, md: 20 }}>{content}</Typography>
+        <Typography fontSize={{ xs: 10, md: 15, lg: 20 }}>{content}</Typography>
       </Box>
     </Box>
   );
