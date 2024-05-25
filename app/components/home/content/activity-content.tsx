4c48074c79b1f3f39b7eb3729c47a8eb5e4ff63d
yusei53 <yusei530922@gmail.com>
2024-05-26 08:42:16 +0900

    contentディレクトリの作成

---
+++ app/components/home/content/activity-content.tsx
@@ -0,0 +1,33 @@
+import { Box, Grid, Typography } from "@mui/material";
+import Heading from "../../common/heading";
+import ActivityCard from "./activity-card";
+import { activityContentData } from "../../../const/activity-content-data";
+
+const ActivityContent = () => {
+  return (
+    <Box bgcolor={"#FFF4FC"} py={10} px={{ xs: 5, md: 30 }}>
+      <Heading title="活動内容" />
+      <Grid container spacing={{ xs: 2, md: 5 }} pt={5}>
+        {activityContentData.map((data) => (
+          <Grid
+            key={data.title}
+            item
+            xs={6}
+            md={4}
+            display={"flex"}
+            alignItems={"center"}
+            justifyContent={"center"}
+          >
+            <ActivityCard
+              src={data.src}
+              title={data.title}
+              content={data.content}
+            />
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
+  );
+};
+
+export default ActivityContent;
