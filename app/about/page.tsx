961dff7789ae8f00437ed150e91f756f2ff81370
Hikari <uj242085@gmail.com>
2024-05-28 22:46:51 +0900

    背景画像を表示させた

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,24 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Image from "next/image";
+
 const page = () => {
-  return <div>about</div>;
+  return (
+    <>
+      <Box sx={{ width: "100%" }}>
+        <Image
+          src="/1920x320.png"
+          width={1920}
+          height={320}
+          style={{
+            maxWidth: "100%",
+            height: "auto",
+          }}
+          alt="Picture of the author"
+        />
+      </Box>
+    </>
+  );
 };
 
 export default page;
