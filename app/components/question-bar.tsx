b77a7dd6927992227955b7e70b9edd4984a35155
togoai0930@icloud.com <togoai0930@icloud.com>
2024-05-28 18:59:58 +0900

    変更ミスを修正した

--- app/components/question-bar.tsx
+++ app/components/question-bar.tsx
@@ -48,12 +48,17 @@ const QuestionBar = () => {
             expandIcon={<ChevronRightIcon sx={{ mr: 1 }} />}
             aria-controls="panel1-content"
             id="panel1-header"
-            sx={{ flexDirection: "row-reverse" }}
+            sx={{
+              flexDirection: "row-reverse",
+              "& .MuiAccordionSummary-expandIconWrapper.Mui-expanded": {
+                transform: "rotate(90deg)",
+              },
+            }}
           >
-            <Typography>質問1</Typography>
+            <Typography>質問１</Typography>
           </AccordionSummary>
           <AccordionDetails>
-            <Typography>
+            <Typography fontSize={{ xs: 13, sm: 15 }}>
               当社は、株主の皆さまに対する利益還元を経営の重要課題として位置づけております。
               株主還元につきましては、事業成長に向けた投資と資本効率向上の最適なバランスを考慮し、総還元性向50％以上を基本方針といたします。配当に関しては、安定的な配当を実現するための指標としてＤＯＥ（株主資本配当率）３％以上を配当方針に据え、過去の配当実績も考慮しながら具体的な配当額を決定いたします
             </Typography>
@@ -74,7 +79,7 @@ const QuestionBar = () => {
             <Typography>質問１</Typography>
           </AccordionSummary>
           <AccordionDetails>
-            <Typography>
+            <Typography fontSize={{ xs: 13, sm: 15 }}>
               当社は、株主の皆さまに対する利益還元を経営の重要課題として位置づけております。
               株主還元につきましては、事業成長に向けた投資と資本効率向上の最適なバランスを考慮し、総還元性向50％以上を基本方針といたします。配当に関しては、安定的な配当を実現するための指標としてＤＯＥ（株主資本配当率）３％以上を配当方針に据え、過去の配当実績も考慮しながら具体的な配当額を決定いたします
             </Typography>
@@ -95,7 +100,7 @@ const QuestionBar = () => {
             <Typography>質問１</Typography>
           </AccordionSummary>
           <AccordionDetails>
-            <Typography>
+            <Typography fontSize={{ xs: 13, sm: 15 }}>
               当社は、株主の皆さまに対する利益還元を経営の重要課題として位置づけております。
               株主還元につきましては、事業成長に向けた投資と資本効率向上の最適なバランスを考慮し、総還元性向50％以上を基本方針といたします。配当に関しては、安定的な配当を実現するための指標としてＤＯＥ（株主資本配当率）３％以上を配当方針に据え、過去の配当実績も考慮しながら具体的な配当額を決定いたします
             </Typography>
