979e3123c1a6c8ba4e7976ddcbc76d05ac88c2ee
Hikari <uj242085@gmail.com>
2024-06-04 18:22:14 +0900

    SubHeaderを追加

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,22 +1,10 @@
 import * as React from "react";
-import Box from "@mui/material/Box";
-import Image from "next/image";
+import SubHeader from "../components/about/subheader";
 
 const page = () => {
   return (
     <>
-      <Box sx={{ width: "100%" }}>
-        <Image
-          src="/1920x320.png"
-          width={1920}
-          height={320}
-          style={{
-            maxWidth: "100%",
-            height: "auto",
-          }}
-          alt="Picture of the author"
-        />
-      </Box>
+      <SubHeader />
     </>
   );
 };
