29265d233980f78710742918e0c4cd8123ec0d71
sora-00 <sora.umiushi@gmail.com>
2024-06-08 16:32:50 +0900

    レスポンシブ対応のlgについて削除

--- app/components/common/footer.tsx
+++ app/components/common/footer.tsx
@@ -27,38 +27,26 @@ const Footer = () => {
           flexDirection={{ xs: "column", md: "row" }}
           spacing={{ xs: 2, md: 5 }}
         >
-          <Grid item xs={12} md={6} lg={4.5}>
-            <Typography fontSize={{ xs: 12, md: 16, lg: 22 }}>
+          <Grid item xs={12} md={6}>
+            <Typography fontSize={{ xs: 12, md: 16 }}>
               成蹊大学プログラミングサークル
               <br />
               Peach Tech
             </Typography>
 
-            <Typography
-              mt={1}
-              color={"gray"}
-              fontSize={{ xs: 10, md: 12, lg: 16 }}
-            >
+            <Typography mt={1} color={"gray"} fontSize={{ xs: 10, md: 12 }}>
               〒170-8633 <br />
               東京都武蔵野市吉祥寺北町３丁目３−１
             </Typography>
           </Grid>
-          <Grid item xs={12} md={6} lg={2.5}>
-            <Typography fontSize={{ xs: 12, md: 16, lg: 22 }}>
-              Contact Us
-            </Typography>
-            <Link
-              href="/"
-              sx={{ color: "gray" }}
-              fontSize={{ xs: 10, md: 12, lg: 16 }}
-            >
+          <Grid item xs={12} md={6}>
+            <Typography fontSize={{ xs: 12, md: 16 }}>Contact Us</Typography>
+            <Link href="/" sx={{ color: "gray" }} fontSize={{ xs: 10, md: 12 }}>
               お問い合わせ
             </Link>
           </Grid>
-          <Grid item xs={12} md={6} lg={2.5}>
-            <Typography fontSize={{ xs: 10, md: 16, lg: 20 }}>
-              Follow Us
-            </Typography>
+          <Grid item xs={12} md={6}>
+            <Typography fontSize={{ xs: 10, md: 16 }}>Follow Us</Typography>
             <Box display={"flex"} justifyContent={"flex-start"} gap={0.5}>
               <Link href="https://x.com/PeachTech_0927">
                 <XIcon sx={{ color: "black" }} />
@@ -68,11 +56,9 @@ const Footer = () => {
               </Link>
             </Box>
           </Grid>
-          <Grid item xs={12} md={6} lg={2.5}>
-            <Typography fontSize={{ xs: 12, md: 16, lg: 22 }}>
-              Developers
-            </Typography>
-            <Typography color={"gray"} fontSize={{ xs: 10, md: 12, lg: 16 }}>
+          <Grid item xs={12} md={6}>
+            <Typography fontSize={{ xs: 12, md: 16 }}>Developers</Typography>
+            <Typography color={"gray"} fontSize={{ xs: 10, md: 12 }}>
               そら
               <br />
               あい
