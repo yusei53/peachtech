405ff07f76c364d1f5b4ef090a5555cfa6f93673
sora-00 <sora.umiushi@gmail.com>
2024-06-12 18:47:13 +0900

    "uselient"の削除/不要なimportの削除/Card→Boxに変更/レスポンシブ対応

--- app/components/home/sponsor/sponsor.tsx
+++ app/components/home/sponsor/sponsor.tsx
@@ -1,20 +1,41 @@
-import { Box, Card, CardMedia, Grid, Typography } from "@mui/material";
+"use client";
+import { Box, styled } from "@mui/material";
 import Heading from "../../common/heading";
+import Image from "next/image";
 
 const Sponsor = () => {
   return (
     <Box bgcolor={"#FFF4FC"} py={10} px={{ xs: 5, md: 30 }}>
       <Heading title="Sponsor" />
       <Box display={"flex"} justifyContent={"center"} pt={5}>
-        <Card sx={{ width: { xs: 150, md: 350 }, borderRadius: "20px" }}>
-          <CardMedia
-            sx={{ height: { xs: 150, md: 350 } }}
-            image="/techtrain-logo.jpeg"
+        <Box
+          bgcolor={"white"}
+          borderRadius={10}
+          boxShadow={
+            "rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px"
+          }
+        >
+          <SImage
+            src="/techtrain-skeleton-logo.png"
+            alt="logo"
+            width={300}
+            height={300}
           />
-        </Card>
+        </Box>
       </Box>
     </Box>
   );
 };
+
+const SImage = styled(Image)(({ theme }) => ({
+  [theme.breakpoints.down("md")]: {
+    width: 170,
+    height: 170,
+  },
+  [theme.breakpoints.down("sm")]: {
+    width: 120,
+    height: 120,
+  },
+}));
 
 export default Sponsor;
