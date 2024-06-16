d0ca9a9aac3254e53d83f5c6be42f6a64e8b7b9a
Hikari <uj242085@gmail.com>
2024-06-16 16:47:09 +0900

    Missonの追加

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,6 @@
 import SubHeader from "../components/about/subheader";
 import AboutBarArea from "../components/about/AboutBarArea";
+import AboutValueDescription from "../components/about/AboutValueDescription";
 
 const page = () => {
   return (
@@ -8,6 +9,13 @@ const page = () => {
       <AboutBarArea
         title={"Vision"}
         subTitle={"Vision"}
+        description={
+          "PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観"
+        }
+      />
+      <AboutBarArea
+        title={"Misson"}
+        subTitle={"Misson"}
         description={
           "PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観"
         }
