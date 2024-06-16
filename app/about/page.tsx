721ac98b548773dc223631a928dfc79dd652a2bb
Hikari <uj242085@gmail.com>
2024-06-16 11:43:47 +0900

    visionの追加

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,10 +1,12 @@
 import * as React from "react";
 import SubHeader from "../components/about/subheader";
+import Vision from "../components/about/vision";
 
 const page = () => {
   return (
     <>
       <SubHeader />
+      <Vision />
     </>
   );
 };
