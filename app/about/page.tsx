fa311dc9744024f655f1b07768e6f788f3edcb9b
Hikari <uj242085@gmail.com>
2024-06-27 07:38:47 +0900

    map対応

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,7 @@
 import SubHeader from "../components/about/subheader";
 import AboutBarArea from "../components/about/AboutBarArea";
 import AboutCommentArea from "../components/about/AboutCommentArea";
+import { missionData, visionData, valueData } from "../const/about-page-data";
 
 const page = () => {
   return (
@@ -8,20 +9,16 @@ const page = () => {
       <SubHeader
         title={"PeachTechの価値観"}
         description={
-          "PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観"
+          " PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観"
         }
       />
-      <AboutBarArea
-        title={"Vision"}
-        subTitle={"Vision"}
-        description={
-          "PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観"
-        }
-      />
+      <AboutBarArea title={"Vision"} aboutPageData={visionData} />
+      <AboutBarArea title={"Misson"} aboutPageData={missionData} />
+      <AboutBarArea title={"Values"} aboutPageData={valueData} />
       <AboutCommentArea
-        title={"代表よりメッセージ"}
+        title={"代表からのメッセージ"}
         comment={
-          "PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観"
+          "PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観"
         }
       />
     </>
