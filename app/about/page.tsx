626fb14cc057d8555be9f54e828d2808c091b182
Hikari <uj242085@gmail.com>
2024-06-26 19:05:30 +0900

    必要なコードの追加

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,15 +1,27 @@
 import SubHeader from "../components/about/subheader";
 import AboutBarArea from "../components/about/AboutBarArea";
+import AboutCommentArea from "../components/about/AboutCommentArea";
 
 const page = () => {
   return (
     <>
-      <SubHeader />
+      <SubHeader
+        title={"PeachTechの価値観"}
+        description={
+          "PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観"
+        }
+      />
       <AboutBarArea
         title={"Vision"}
         subTitle={"Vision"}
         description={
           "PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観"
+        }
+      />
+      <AboutCommentArea
+        title={"代表よりメッセージ"}
+        comment={
+          "PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観"
         }
       />
     </>
