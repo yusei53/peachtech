709aecbe1888cefe637fe0052f836f5f3975ace3
yusei53 <yusei530922@gmail.com>
2024-06-26 19:05:57 +0900

    /aboutのpage.tsxをリファクタ

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,50 +1,14 @@
 import SubHeader from "../components/about/subheader";
 import AboutBarArea from "../components/about/AboutBarArea";
+import { missionData, valueData, visionData } from "../const/about-page-data";
 
 const page = () => {
   return (
     <>
       <SubHeader />
-      <AboutBarArea
-        title={"Vision"}
-        subTitle={"Vision"}
-        description={
-          "PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観"
-        }
-        displayValues={true}
-      />
-      <AboutBarArea
-        title={"Misson"}
-        subTitle={"Misson"}
-        description={
-          "PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観"
-        }
-        displayValues={true}
-      />
-      <AboutBarArea
-        title={"Values"}
-        subTitle={"Value"}
-        description={
-          "PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観"
-        }
-        displayValues={true}
-      />
-      <AboutBarArea
-        title={""}
-        subTitle={"Value"}
-        description={
-          "PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観"
-        }
-        displayValues={false}
-      />
-      <AboutBarArea
-        title={""}
-        subTitle={"Value"}
-        description={
-          "PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観"
-        }
-        displayValues={false}
-      />
+      <AboutBarArea title={"Vision"} aboutPageData={visionData} />
+      <AboutBarArea title={"Misson"} aboutPageData={missionData} />
+      <AboutBarArea title={"Values"} aboutPageData={valueData} />
     </>
   );
 };
