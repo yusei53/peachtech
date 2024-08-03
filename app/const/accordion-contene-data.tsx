2ff524b8b2a1018e505c112f5bf587dd715622fd
yuto99999 <myuto2003@gmail.com>
2024-08-03 16:17:14 +0900

    質問内容追加

--- app/const/accordion-contene-data.tsx
+++ app/const/accordion-contene-data.tsx
@@ -5,20 +5,43 @@ export type accordionContentDataProps = {
 
 export const accordionContentData: accordionContentDataProps[] = [
   {
-    accordionTitle: "初心者でも大丈夫ですか",
-    description: "かかきくけこあああああああああああああ",
+    accordionTitle: "初心者でも大丈夫ですか？",
+    description: "大丈夫です！未経験から初めた人もたくさんいます！！",
   },
   {
-    accordionTitle: "どんなサークルですか",
-    description: "かかきくけこああああああああああああああああああ",
-  },
-  {
-    accordionTitle: "男女比はどのくらいですか",
+    accordionTitle: "文系でも入れますか？",
     description:
-      "かかきくけこあああああああああああああああああああああああああ",
+      "入れます！！文系:理系は3:7くらいです！ちなみに代表は2年連続文系です！",
   },
   {
-    accordionTitle: "週にどのくらい活動してますか",
-    description: "かかきくけこああああああああああああああああああ",
+    accordionTitle: "男女比はどのくらいですか？",
+    description: "7:3くらいです！(女子の人数は年々増えています！)",
+  },
+  {
+    accordionTitle: "PeachTechには何人程度の人が在籍していますか？",
+    description: "100人ほど在籍しています！",
+  },
+  {
+    accordionTitle: "週にどのくらい活動していますか？",
+    description:
+      "主にDiscordを利用してオンラインで活動しています！また大学のある期間は週に1回対面活動を行っています！",
+  },
+  {
+    accordionTitle: "会費はありますか？",
+    description: "会費はありません！",
+  },
+  {
+    accordionTitle: "参加するにあたって必要な条件はありますか？",
+    description: "特にありません！パッションがあると嬉しいです！笑",
+  },
+  {
+    accordionTitle: "サークル･部活と掛け持ちできますか？",
+    description:
+      "できます！！サークルや部活動に関わらず掛け持ちしている人はたくさんいます！",
+  },
+  {
+    accordionTitle: "どのような分野に興味のある学生が集まっていますか？",
+    description: `現在はwebサイトやwebアプリを開発できるwebプログラミングに興味を持っているメンバーが多いです！
+    他にもゲームやデータ分析などに興味を持っているメンバーもいるので、それらの領域も深めていきたいと考えています！`,
   },
 ];
