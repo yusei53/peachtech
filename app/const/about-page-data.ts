2d6c45de27c8a2f8842540f48241d7327eb3f011
yusei53 <yusei530922@gmail.com>
2024-08-04 23:07:24 +0900

    update: vision

--- app/const/about-page-data.ts
+++ app/const/about-page-data.ts
@@ -5,9 +5,9 @@ export type aboutDataProps = {
 
 export const visionData: aboutDataProps[] = [
   {
-    subTitle: "vision",
+    subTitle: "共創して、楽しいを生み出す",
     description:
-      "PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観PeachTechの価値観",
+      "私たちのプログラミングサークルは、楽しむことを軸に技術と向き合います。エンジニアリングのスキルを磨くだけでなく、ITの力を活用して世の中を豊かにし、身近な人々に喜びや幸せを届けることを目指します。同じ目標を持った仲間と繋がりながらも、それぞれの個性と強みを活かしながら、一人では成し得ないことを仲間と共に実現し、プログラミングを通じて楽しく成長する場を実現させたいです。",
   },
 ];
 
