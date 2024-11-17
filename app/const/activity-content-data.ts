d61893f71f8474c86bab733b775e2f8f8d73d149
ryolingo <ryomatsuya04@gmail.com>
2024-11-17 19:34:05 +0900

    活動内容を記入

--- app/const/activity-content-data.ts
+++ app/const/activity-content-data.ts
@@ -7,32 +7,37 @@ export type activityContentDataProps = {
 export const activityContentData: activityContentDataProps[] = [
   {
     src: "/icon_157130_256.png",
-    title: "オンライン活動",
-    content: "あいうえおあいうえおあいうえおあいうえおdddddd",
+    title: "オンライン・対面活動",
+    content:
+      "Discordを使ってオンラインで活動し、週に一回対面でワイワイ開発をしています!",
   },
   {
     src: "/icon_152770_256.png",
     title: "勉強会、LT会",
-    content: "あいうえおあいうえおあいうえおあいうえおdddddd",
+    content:
+      "使いたい技術の共有や最新知識を学ぶ勉強会、LT会などを積極的に行なっております!",
   },
   {
     src: "/icon_158210_256.png",
     title: "チーム開発",
-    content: "あいうえおあいうえおあいうえおあいうえおdddddd",
+    content:
+      "ゲーム・Webサイト・Webアプリ・電子工作などを同期や先輩たちと開発をしています!",
   },
   {
     src: "/icon_144440_256.png",
     title: "他大学、企業交流",
-    content: "あいうえおあいうえおあいうえおあいうえおdddddd",
+    content:
+      "様々なIT企業や、他大学のプログラミング学生団体と交流し、共に成長できる場を作っています!",
   },
   {
     src: "/icon_001950_256.png",
-    title: "就活サポート",
-    content: "あいうえおあいうえおあいうえおあいうえおdddddd",
+    title: "就活相談",
+    content:
+      "就活エージェントの紹介・面接練習・インターンの紹介等、先輩からの手厚いサポートがあります！",
   },
   {
     src: "/icon_141950_256.png",
     title: "イベント",
-    content: "あいうえおあいうえおあいうえおあいうえおdddddd",
+    content: `開発合宿や欅祭での出展、その他プログラミング以外の楽しいイベントもあります!`,
   },
 ];
